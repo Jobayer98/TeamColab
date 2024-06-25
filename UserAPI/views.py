3e5cc830a0a8c4be49977b91f9dac25f038991e2
Jobayer98 <joobayerrahman0220@gmail.com>
2024-06-25 08:03:29 +0600

    added registration view

--- UserAPI/views.py
+++ UserAPI/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status, views
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from .models import CustomUser as User
+from .serializers import RegistrationSerializer, UserSerializer
+
+@api_view(['POST'])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            
+            refresh = RefreshToken.for_user(user)
+            token = {
+                "token": {
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token)
+                }
+            }
+            return Response(token, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# def login_view()
+
+
+class UserView(views.APIView):
+    pass
--- team_colab/urls.py
+++ team_colab/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('ProjectAPI.urls')),
+    path('api/users/', include('UserAPI.urls')),
     path('admin/', admin.site.urls),
 ]
